1fd2a1d053e2786ec83fd7ed7b027ce7dac96adc
Jordi Vidal <163132865+reivaX1755@users.noreply.github.com>
2025-04-03 21:06:43 +0200

    changed dominion to ip

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    MYSQL_HOST = os.getenv("DB_HOST", "sql.freedb.tech")
+    MYSQL_HOST = os.getenv("DB_HOST", "130.162.54.212")
     MYSQL_USER = os.getenv("DB_USER", "freedb_unityquestdb_admin")
     MYSQL_PASSWORD = os.getenv("DB_PASS", "bNzQ@rYgFb2tqga")
     MYSQL_PORT = int(os.getenv("DB_PORT", 3306))
